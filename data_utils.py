2db1a9d7c149314b20e8440be1df093569d01d70
moisestg <moisestg93@gmail.com>
2017-06-23 09:59:54 +0200

    Reverse top-k predicitions

--- data_utils.py
+++ data_utils.py
@@ -313,7 +313,7 @@ def eval_last_word_detailed(session, model, input_data, id2word, pos):
 				# Top k predictions
 				relevant_logits = logits[b, relevant_indexes[b], :] # [vocab_size]
 				ordered_indexes = relevant_logits.argsort() # from less to more
-				topk_indexes = ordered_indexes[-10:]
+				topk_indexes = ordered_indexes[-10:][::-1]
 				f.write("Top 10 predictions:")
 				for index in topk_indexes:
 					f.write(" "+id2word[index])
@@ -448,7 +448,8 @@ def eval_last_word_cache_detailed(session, model, input_data, id2word, pos):
 		results = session.run(fetches, feed_dict)
 		rnn_outputs = results["outputs"] # list of "max_len" np arrays of [batch_size, hidden_size]
 		logits = results["logits"] # np.array of [batch_size*max_len, vocab_size]
-		logits = np.reshape(logits, (input_x.shape[0], -1, logits.shape[1])) # [batch_size, max_len, vocab_size]
+		vocab_size = logits.shape[1]
+		logits = np.reshape(logits, (input_x.shape[0], -1, vocab_size)) # [batch_size, max_len, vocab_size]
 
 		relevant_indexes = np.apply_along_axis(relevant_index, 1, input_x) # [batch_size]
 		last_word_indexes = relevant_indexes + 1
@@ -464,64 +465,69 @@ def eval_last_word_cache_detailed(session, model, input_data, id2word, pos):
 
 		# DETAILED STUFF PER EXAMPLE
 		all_word_probs = np.apply_along_axis(softmax, 2, logits) # [batch_size, max_len, vocab_size]
-		for b in range(input_x.shape[0]): # batch_size
-			f.write("* EXAMPLE "+str(example_count)+":\n")
-			# Target word info
-			target_word_id = input_y[b, relevant_indexes[b]]
-			f.write("Target word: "+id2word[ target_word_id ]+" | PoS tag: "+pos[example_count]+"\n")
-			cache_logits = dict() # key: output word, value: logit	
-			# Update all probs by mixing word + cache	
-			for i in range(1, relevant_indexes[b]+2): # +1 ???? until last word
-				# Calculate cache probabilities
-				h_t = rnn_outputs[i][b,:]
-				pseudo_logit = np.exp( theta*np.sum( h_t*rnn_outputs[i-1][b,:]) )
-				output_id = input_x[b,:][i]
-				if output_id in cache_logits: 
-					cache_logits[output_id] += pseudo_logit
-				else:
-					cache_logits[output_id] = pseudo_logit
+		with open("detailed_output_"+str(int(start_time))+".txt", "a") as f:
+			for b in range(input_x.shape[0]): # batch_size
+				f.write("* EXAMPLE "+str(example_count)+":\n")
+				# Target word info
+				target_word_id = input_y[b, relevant_indexes[b]]
+				f.write("Target word: "+id2word[ target_word_id ]+" | PoS tag: "+pos[example_count]+"\n")
+				cache_logits = dict() # key: output word, value: logit	
+				# Update all probs by mixing word + cache	
+				for i in range(1, relevant_indexes[b]+2): # +1 ???? until last word
+					# Calculate cache probabilities
+					h_t = rnn_outputs[i][b,:]
+					pseudo_logit = np.exp( theta*np.sum( h_t*rnn_outputs[i-1][b,:]) )
+					output_id = input_x[b,:][i]
+					if output_id in cache_logits: 
+						cache_logits[output_id] += pseudo_logit
+					else:
+						cache_logits[output_id] = pseudo_logit
 
-				total_sum = sum(cache_logits.values())
-				cache_probs = [float(val)/float(total_sum) for val in cache_logits.values()]
-				cache_ids = cache_logits.keys()
+					total_sum = sum(cache_logits.values())
+					cache_probs = [float(val)/float(total_sum) for val in cache_logits.values()]
+					cache_ids = cache_logits.keys()
 
-				# Merge word (RNN) and cache probabilities
-				all_word_probs[b, i, :] = (1-interpol)*all_word_probs[b, i, :] # [vocab_size]
-				for j, output_id in enumerate(cache_ids):
-					all_word_probs[b, i, output_id] += interpol*cache_probs[j]
-			# Top k predictions
-			relevant_probs = all_word_probs[b, relevant_indexes[b], :] # [vocab_size]
-			ordered_indexes = relevant_probs.argsort() # from less to more
-			topk_indexes = ordered_indexes[-10:]
-			f.write("Top 10 predictions:")
-			for index in topk_indexes:
-				f.write(" "+id2word[index])
-			f.write("\n")
-			# Word perplexities: word/perplexity when predicting that word (the final prediction might have been different than the target)
-			f.write("Word perplexities:\n")
-			for i in range(relevant_indexes[b]+2): # until the last word
-				f.write(id2word[ input_x[b, i] ]) # word
-				if i>0:
-					f.write("/"+str( round(np.exp(-np.log( all_word_probs[b, i-1, target_word_id] )) , 2))+" ")
-				else:
-					f.write(" ")
-			f.write("\n\n")
-			example_count += 1
+					# Merge word (RNN) and cache probabilities
+					all_word_probs[b, i, :] = (1-interpol)*all_word_probs[b, i, :] # [vocab_size]
+					for j, output_id in enumerate(cache_ids):
+						all_word_probs[b, i, output_id] += interpol*cache_probs[j]
+				# Top k predictions
+				relevant_probs = all_word_probs[b, relevant_indexes[b], :] # [vocab_size]
+				ordered_indexes = relevant_probs.argsort() # from less to more
+				topk_indexes = ordered_indexes[-10:][::-1]
+				f.write("Top 10 predictions:")
+				for index in topk_indexes:
+					f.write(" "+id2word[index])
+				f.write("\n")
+				# Target word rank
+				target_word_rank = vocab_size - np.where(ordered_indexes == target_word_id)[0][0]
+				ranks = np.append(ranks, target_word_rank)
+				f.write("Target word rank: "+str(target_word_rank)+"\n")
+				# Word perplexities: word/perplexity when predicting that word (the final prediction might have been different than the target)
+				f.write("Word perplexities:\n")
+				for i in range(relevant_indexes[b]+2): # until the last word
+					f.write(id2word[ input_x[b, i] ]) # word
+					if i>0:
+						f.write("/"+str( round(np.exp(-np.log( all_word_probs[b, i-1, target_word_id] )) , 2))+" ")
+					else:
+						f.write(" ")
+				f.write("\n\n")
+				example_count += 1
 
-			# Calculate loss
-			loss = -np.log( all_word_probs[b, relevant_indexes[b], target_word_id] )
-			losses = np.append(losses, loss)
+				# Calculate loss
+				loss = -np.log( all_word_probs[b, relevant_indexes[b], target_word_id] )
+				losses = np.append(losses, loss)
 
-			# And accuracy
-			predicted_id = np.argmax(all_word_probs[b, relevant_indexes[b], :])
-			accuracies = np.append( accuracies, predicted_id == target_word_id )
+				# And accuracy
+				predicted_id = np.argmax(all_word_probs[b, relevant_indexes[b], :])
+				accuracies = np.append( accuracies, predicted_id == target_word_id )
 
 	perplexity = np.exp(np.mean(losses))
 	accuracy = np.mean(accuracies) 
 	rank = np.median(ranks)
 	with open("detailed_output_"+str(int(start_time))+".txt", "a") as f:
-		f.write("Average (target word) perplexity: "+str(perplexity)+" | Average (target word) accuracy: "+str(accuracy))
-		#	+" | Median (target word) rank: "+str(rank))
+		f.write("Average (target word) perplexity: "+str(perplexity)+" | Average (target word) accuracy: "+str(accuracy)
+				+" | Median (target word) rank: "+str(rank))
 
 	print("Detailed eval time: "+str(time.time()-start_time)+" s") 
 
